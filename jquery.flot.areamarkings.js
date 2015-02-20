78c06161841445e877fbc8171fbf995391b51c0f
Michel de Nijs <micheldenijs@mail.com>
2015-02-20 10:59:43 +0100

    Update jquery.flot.areamarkings.js

--- jquery.flot.areamarkings.js
+++ jquery.flot.areamarkings.js
@@ -3,6 +3,7 @@
  * Author(s):   Xinjo/Michel de Nijs
  * Version:     0.0.2
  * Description: A plugin for Flot to create markings of all kinds of shapes easily.
+ * Website:     https://github.com/Xinjo/AreaMarkings
  * 
  */
 
